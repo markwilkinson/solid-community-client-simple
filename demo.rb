6cdd90b0ccff44e081a700ecaee4e1c77ed7e7a6
Mark Wilkinson <markw@illuminae.com>
2024-08-14 16:50:13 +0200

    demo of how to use it

--- demo.rb
+++ demo.rb
@@ -1,5 +1,5 @@
- require "./lib/solid.rb"
-#require "solid-community-client-simple"
+# require "./lib/solid.rb"
+require "solid-community-client-simple"
 s = SOLID::CommunityClient.new(server: "http://localhost:3000/", username: "mark.wilkinson@upm.es", password: "markw")
 s.login
 at = s.get_current_access_token(webid: "http://localhost:3000/markw/profile/card#me", name: "codetest")
